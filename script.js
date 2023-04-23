f054fb15361c384e90b1cb7d7c05aa90c5e1712e
Olatoyan <golatoyan@gmail.com>
2023-04-23 22:55:52 +0100

    new commit

--- script.js
+++ script.js
@@ -9,9 +9,28 @@ const dotContainer = document.querySelector(".dots");
 const submit = document.querySelector(".footer__submit");
 const emailInput = document.querySelector(".footer__email");
 const invalid = document.querySelector(".invalid");
+const header = document.querySelector(".header");
+const navOpen = document.querySelector(".nav-open");
+const openBtn = document.querySelector(".open-icon");
+const closeBtn = document.querySelector(".close-icon");
+const navLinks = document.querySelectorAll(".nav__link");
 
-// testimonialSlider.style.transform = `scale(0.5)`;
-// testimonialSlider.style.overflow = `visible`;
+const overlay = document.querySelector(".overlay");
+
+const closeOverlay = function () {
+  header.classList.remove("nav-open");
+};
+
+openBtn.addEventListener("click", function () {
+  header.classList.add("nav-open");
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", closeOverlay);
+});
+
+closeBtn.addEventListener("click", closeOverlay);
+overlay.addEventListener("click", closeOverlay);
 
 const goToSlide = function (slide) {
   slider.forEach(
